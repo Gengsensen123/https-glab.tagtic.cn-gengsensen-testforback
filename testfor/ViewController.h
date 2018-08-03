6289df0ddfda43c1600c35bdfdb4b873c9f55e76
耿森森 <gengsensen@donews.com>
2018-08-03 10:37:42 +0800

    Initial Commit

---
+++ testfor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testfor
+//
+//  Created by 耿森森 on 2018/8/3.
+//  Copyright © 2018年 耿森森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testfor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testfor
+//
+//  Created by 耿森森 on 2018/8/3.
+//  Copyright © 2018年 耿森森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
